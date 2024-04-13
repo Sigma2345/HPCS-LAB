f90d63cb282c5abc4655934306baa04234641062
Sigma2345 <95545310+Sigma2345@users.noreply.github.com>
2024-04-13 11:54:40 +0530

    concurrent ds

--- lab-8/linkedListInsertAndRemove.cpp
+++ lab-8/linkedListInsertAndRemove.cpp
@@ -15,28 +15,6 @@ public:
     }
 };
 
-// void remove(Node* sup_head){
-
-// }
-
-// Node* contain(Node* sup_head){
-
-// }
-
-pair<Node*, Node*> searchPos(Node* sup_head, int val){
-    Node*h = sup_head ;
-    Node*c = h->next ;
-    // tail and pos not reached
-    while(c->val != -1 && c->val < val){
-        h = h->next; 
-        c = c->next; 
-    }   
-
-    omp_set_lock(&(h->writeLock)); 
-    omp_set_lock(&(c->writeLock)); 
-    return {h,c}; 
-}
-
 void print(Node* sup_head){
     Node* h = sup_head ;
     while(h){
@@ -47,49 +25,88 @@ void print(Node* sup_head){
     return;
 }
 
+Node* contain(Node* sup_head, int val){
+    Node*h = sup_head ;
+    Node*c = h->next ;
+    omp_set_lock(&(h->writeLock)); 
+    omp_set_lock(&(c->writeLock)); 
+    while(c->val!=-1 && c->val != val){
+        omp_set_lock(&(c->next->writeLock)); 
+        omp_unset_lock(&(h->writeLock));
+        c = c->next;
+        h = h->next;
+    }
+    
+    if(c->val == -1){
+        omp_unset_lock(&(h->writeLock)); 
+        omp_unset_lock(&(c->writeLock)); 
+        return NULL; 
+    }
+    return h; 
+    
+}   
+
+void remove(Node* sup_head, int val){
+    #pragma omp critical 
+    {
+        cout<<"Inserting "<<val<<" by thread "<<omp_get_thread_num()<<endl ;
+    }
+    Node*n = new Node(val); 
+    Node* h = contain(sup_head, val);  
+    if(h==NULL){
+        #pragma omp critical 
+        {
+            cout<<val<<" Not in list"<<endl ;
+        }
+        return ;
+    }
+    Node*c = h->next ;
+    h->next = c->next; 
+    c->next = NULL ;
+    omp_unset_lock(&(h->writeLock)); 
+    omp_unset_lock(&(c->writeLock)); 
+    delete c; 
+    #pragma omp critical 
+    {
+        print(sup_head); 
+    }
+}
+
+Node* searchPos(Node* sup_head, int val){
+    Node*h = sup_head ;
+    Node*c = h->next ;
+    // tail and pos not reached
+    omp_set_lock(&(h->writeLock)); 
+    omp_set_lock(&(c->writeLock)); 
+    while(c->val!=-1 && c->val < val){
+        omp_set_lock(&(c->next->writeLock)); 
+        omp_unset_lock(&(h->writeLock));
+        c = c->next;
+        h = h->next;
+    }
+    return h;
+}
+
+
+
 void insert(Node* sup_head, int val){
     #pragma omp critical 
     {
         cout<<"Inserting "<<val<<" by thread "<<omp_get_thread_num()<<endl ;
     }
-    //search for pos
+    Node*n = new Node(val); 
+    Node* h = searchPos(sup_head, val);  
+    Node*c = h->next; 
+    n->next = c; 
+    h->next = n; 
 
-    auto x = searchPos(sup_head, val); 
-    Node*pos = x.first, pos1 = x.second; 
-
-    #pragma omp critical 
-    {
-        if(pos->next != pos1){
-            omp_unset_lock(&(pos->writeLock)); 
-            omp_unset_lock(&(pos1->writeLock));      
-                   
-        }
-
-    }
-
-    // lock pos and nextpos
-    // omp_set_lock(&(pos->writeLock)); 
-    // omp_set_lock(&(next_pos->writeLock)); 
+    omp_unset_lock(&(h->writeLock)); 
+    omp_unset_lock(&(c->writeLock)); 
 
     #pragma omp critical 
     {
-        cout<<"Inserting "<<val<<" by thread "<<omp_get_thread_num()<<" Acquired lock on values"<<endl ;
-    }
-
-    Node* n = new Node(val); 
-    pos->next = n; 
-    n->next = next_pos; 
-
-    // unlock pos and nextpos
-    omp_unset_lock(&(pos->writeLock)); 
-    omp_unset_lock(&(next_pos->writeLock)); 
-
-    #pragma omp critical 
-    {
-        cout<<"Freed locks by thread "<<omp_get_thread_num()<<endl ;
         print(sup_head); 
     }
-
 }
 
 
@@ -101,13 +118,17 @@ int main(){
     Node* sup_head = new Node(-1), *sup_tail = new Node(-1); 
     sup_head->next = sup_tail; 
 
-    int arr[] = {1, 2, 3}; 
+    int arr[] = {1, 5, 2, 3}; 
     #pragma omp parallel for
-    for(int i = 0 ; i<3 ; i++){
+    for(int i = 0 ; i<4 ; i++){
         insert(sup_head, arr[i]); 
     }
-
-    print(sup_head); 
+    int arr1[] = {1,4,3,2}; 
+    #pragma omp parallel for
+    for(int i = 0 ; i<4 ; i++){
+        remove(sup_head, arr1[i]); 
+    }
+    // print(sup_head); 
 
     return 0; 
 }
